3475a48722829b38188dd2bda1bb6bfd46f397c4
Thomas R. Koll <tomk32@tomk32.de>
2008-11-10 11:27:08 +0100

    Added spec for UsersController Started with specs for SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,9 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SessionsController do
 
-  #Delete this example and add some real ones
-  it "should use SessionsController" do
-    controller.should be_an_instance_of(SessionsController)
-  end
+  it "should send request to OpenID provider"
+  
+  it "should accept response from OpenID provider"
+  
+  it "should create user account for new identity with sreg"
+  
+  it "should show user form for identty without sreg"
 
 end
