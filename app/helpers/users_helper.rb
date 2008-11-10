d4ede5e1ba6eb7f1a87081b8cfa3cc86fe8be017
Thomas R. Koll <tomk32@tomk32.de>
2008-11-10 15:18:36 +0100

    Moved current_user and logged_in? from UsersHelper to Authentication module

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,2 @@
 module UsersHelper
-  
-  def current_user
-    @current_user = User.find_by_id(session[:user_id])
-  end
-  
-  def logged_in?
-    ! current_user.nil?
-  end
 end
--- lib/authentication.rb
+++ lib/authentication.rb
@@ -1,9 +1,22 @@
 module Authentication
+  def self.included(base)
+    base.send(:helper_method, :current_user)
+    base.send(:helper_method, :logged_in?)
+  end
+
   def authenticated
-    if (session[:user_id].blank? or User.find_by_id(session[:user_id]).nil?)
+    unless logged_in?
       flash[:error] = "You must be logged in for this action"
       redirect_to login_url and return false
     end
     return true
+  end
+
+  def current_user
+    @current_user = User.find_by_id(session[:user_id])
+  end
+  
+  def logged_in?
+    ! current_user.nil?
   end
 end
