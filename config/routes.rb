280c5c8fd046f38a7e8308674801e590a8f30a08
Thomas R. Koll <tomk32@tomk32.de>
2008-11-10 15:34:49 +0100

    Started with a dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 ActionController::Routing::Routes.draw do |map|
   
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  
+
   map.resources :photos
   map.resources :sources
   map.resources :users
   map.resources :sessions
   map.logout 'logout', :controller => "sessions", :action => "delete"
   map.login 'login', :controller => "sessions", :action => "new"
+
+  map.connect 'dashboard', :controller => 'dashboard'
+
   map.root :controller => 'photos'
 
   # See how all your routes lay out with "rake routes"
