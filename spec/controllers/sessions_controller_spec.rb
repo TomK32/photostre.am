81fd109b43162335f99cc0599ed8b6f97821f987
Thomas R. Koll <tomk32@tomk32.de>
2008-11-10 14:57:07 +0100

    Ctarted with SessionsController spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,8 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SessionsController do
+  integrate_views
 
-  it "should send request to OpenID provider"
+  it "should have authenticate form send request to OpenID provider" do
+    get :new
+    response.should have_tag('input[type=?]#openid_identifier', 'text')
+    post :create, :openid_identifier => 'http://tomk32.myopenid.com'
+    response.should be_redirect
+  end
   
   it "should accept response from OpenID provider"
   
