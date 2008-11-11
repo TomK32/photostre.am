1a92ab2b28e99290f3570701762ff273486846f5
Thomas R. Koll <tomk32@tomk32.de>
2008-11-11 11:36:04 +0100

    started with SourcesController

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,2 +1,10 @@
 class SourcesController < ApplicationController
+  before_filter :authenticated
+
+  def index
+    @sources = current_user.sources
+  end
+  def new
+    @source = Source.new
+  end
 end
