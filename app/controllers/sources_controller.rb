44d40557e94ccd0f52042e1e62a56966e0cd2c0a
Thomas R. Koll <tomk32@tomk32.de>
2008-11-11 11:45:10 +0100

    Added forms for sources Added SourcesController#create

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -4,7 +4,18 @@ class SourcesController < ApplicationController
   def index
     @sources = current_user.sources
   end
+
   def new
     @source = Source.new
+  end
+
+  def create
+    @source = current_user.sources.new(params[:source])
+    if @source.save
+      redirect_to source_url(@source)
+    else
+      flash[:error] = "Could not save the new source"
+      render :action => :new and return
+    end
   end
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -2,10 +2,18 @@ class Source < ActiveRecord::Base
   has_many :photos
   belongs_to :user
   
-  ACTIVE_TYPES = [Flickr, 'Flickr.com']
+  ACTIVE_TYPES = AVAILABLE_TYPES = [['Flickr.com', Flickr]]
   
   validates_presence_of :type
   validates_presence_of :user_id
   validates_presence_of :title
+  
+  def source_type=(source_type)
+    self.type = source_type if AVAILABLE_TYPES.collect{|t|t[0]}.include?(source_type)
+  end
+
+  def source_type
+    self.type.to_s
+  end
   
 end
