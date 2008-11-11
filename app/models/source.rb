9345104442b0e844ccbcdb68e7fe4f3cb709d0bf
Thomas R. Koll <tomk32@tomk32.de>
2008-11-11 11:35:03 +0100

    Added Source submodel Flickr

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,4 +2,10 @@ class Source < ActiveRecord::Base
   has_many :photos
   belongs_to :user
   
+  ACTIVE_TYPES = [Flickr, 'Flickr.com']
+  
+  validates_presence_of :type
+  validates_presence_of :user_id
+  validates_presence_of :title
+  
 end
