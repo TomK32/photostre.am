bf1b4aa1f8a1463012144291e5904c2ea8e951a1
Thomas R. Koll <tomk32@tomk32.de>
2008-11-18 18:40:08 +0100

    working on photos#index and #show and dashboard#index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,16 @@
 class PhotosController < ApplicationController
+  
+  before_filter :current_photo, :only => :show
+
   def index
-    @photos = Photo.active.paginate(:limit => params[:limit], :page => params[:page])
+    @photos = Photo.published.paginate(:limit => params[:limit], :page => params[:page])
+  end
+  
+  def show
+  end
+  
+  private
+  def current_photo
+    @photo ||= Photo.find(params[:id])
   end
 end
