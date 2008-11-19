f6572c6676247db6ccf2766961a7b34416fa58a1
Thomas R. Koll <tomk32@tomk32.de>
2008-11-19 17:29:50 +0100

    started with photos#edit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,20 @@
 class PhotosController < ApplicationController
   
-  before_filter :current_photo, :only => :show
+  before_filter :current_photo, :only => [:show, :edit]
+  caches_action :show, :index
 
   def index
     @photos = Photo.published.paginate(:limit => params[:limit], :page => params[:page])
   end
   
   def show
+  end
+  
+  def edit
+    if @photo.user_id != current_user.id
+      flash[:error] = "You cannot edit this photo"
+      redirect_to photo_url(photo) and return
+    end
   end
   
   private
