bf824fcdb21ee7b56b123ea42150f83a9d14289e
Thomas R. Koll <tomk32@tomk32.de>
2008-11-20 13:46:53 +0100

    working on layout. added helper for meta_keywords

--- app/helpers/website_helper.rb
+++ app/helpers/website_helper.rb
@@ -1,7 +1,13 @@
 module WebsiteHelper
-  attr_accessor :site_title, :page_title
+  attr_accessor :site_title, :page_title, :meta_keywords
 
   def site_title
     "%s // %s" % [page_title, current_website.site_title]
+  end
+  
+  def meta_keywords
+    @meta_keywords.split(',') if @meta_keywords.is_a?(String)
+    keywords = [@meta_keywords, current_website.meta_keywords.split(',')].compact
+    keywords.map{|k|k.}.join(',')
   end
 end
