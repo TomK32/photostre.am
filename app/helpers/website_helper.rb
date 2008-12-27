6fe00e7a0ab99a1df3fbce1df0fa0fb02b93fee4
Thomas R. Koll <tomk32@tomk32.de>
2008-12-27 22:32:02 +0100

    fixed site_title helper

--- app/helpers/website_helper.rb
+++ app/helpers/website_helper.rb
@@ -2,12 +2,13 @@ module WebsiteHelper
   attr_accessor :site_title, :page_title, :meta_keywords
 
   def site_title
-    "%s // %s" % [page_title, current_website.site_title]
+    [page_title, current_website.site_title].compact.join(' // ')
   end
   
   def meta_keywords
-    @meta_keywords.split(',') if @meta_keywords.is_a?(String)
-    keywords = [@meta_keywords, current_website.meta_keywords.split(',')].compact
-    keywords.map{|k|k.}.join(',')
+    @meta_keywords = @meta_keywords.split(',') if @meta_keywords.is_a?(String)
+    @meta_keywords ||= []
+    keywords = (@meta_keywords + current_website.meta_keywords.split(',')).compact
+    keywords.map{|k| k.squish }.join(',')
   end
 end
