bf2bdb4a6612e10301579d120f1354f39d08e242
Thomas R. Koll <tomk32@tomk32.de>
2009-01-16 00:19:30 +0100

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081120110036) do
+ActiveRecord::Schema.define(:version => 20090115224542) do
 
   create_table "albums", :force => true do |t|
     t.datetime "created_at"
@@ -88,19 +88,21 @@ ActiveRecord::Schema.define(:version => 20081120110036) do
   add_index "sources", ["type"], :name => "index_sources_on_type"
 
   create_table "taggings", :force => true do |t|
-    t.string   "tagger_type"
-    t.integer  "tagger_id"
-    t.string   "taggable_type"
+    t.integer  "tag_id"
     t.integer  "taggable_id"
-    t.string   "tag"
-    t.string   "normalized"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "taggable_type"
     t.string   "context"
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
   add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-  add_index "taggings", ["taggable_id", "taggable_type", "context", "normalized"], :name => "taggable_and_context_and_normalized"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",      :limit => 40,  :default => "",        :null => false
