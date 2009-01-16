924200fc77c2665b71f74be0a2eabcba46def7a9
Thomas R. Koll <tomk32@tomk32.de>
2009-01-16 18:16:21 +0100

    improved photos_controller_spec a bit

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -12,19 +12,27 @@ describe PhotosController do
     end
   end
   describe "GET to :show" do
+    before :each do
+      @photo = Photo.find(1)
+    end
     it "should assign @photo for id" do
       get :show, :id => '1'
-      assigns[:photo].should == Photo.find(1)
+      assigns[:photo].should == @photo
     end
     it "should assign @photo for permalink" do
       get :show, :id => 'self-portrait'
-      assigns[:photo].should == Photo.find(1)
+      assigns[:photo].should == @photo
     end
     it "should redirect to :index for non-existing photo" do
       get :show, :id => 'no-such-photo'
       assert_not_nil flash[:error]
       assert_response 404
       assert_template 'photos/index'
+    end
+    it "should assign metatags and title" do
+      get :show, :id => '1'
+      assert assigns['meta_keywords'] == @photo.tag_list
+      assert assigns['page_title'] == @photo.title
     end
   end
 end
