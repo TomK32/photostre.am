f0f303c4a5c995d4fcbc25a3667945d3abef94fb
Thomas R. Koll <tomk32@tomk32.de>
2009-06-09 23:30:59 +0200

    Migrated dashboard view to HAML

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   before_filter :authenticated
   def index
     @user = current_user
+    @photos = @user.photos.recent.all(:limit => 8)
+    @websites = current_user.websites
   end
 end
