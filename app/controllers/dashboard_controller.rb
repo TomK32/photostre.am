4ee255731905075919f3fbbe47b98b523c9f7dec
Thomas R. Koll <tomk32@tomk32.de>
2009-09-03 00:10:51 +0200

    Shufflig dashboard Added #info to application layout Some smaller fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_filter :authenticated
   def index
     @user = current_user
+    @sources = @user.sources
     @photos = @user.photos.recent.all(:limit => 8)
     @websites = current_user.websites
   end
--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem 'haml-edge', :lib => 'haml', :version => '>= 2.0.6'
   config.gem 'chriseppstein-compass', :lib => 'compass'
+  config.gem 'RedCloth'
 #  config.gem 'chriseppstein-compass_960_plugin', :lib => 'compass-960-plugin'
 #  config.gem 'xml_magic'
 
