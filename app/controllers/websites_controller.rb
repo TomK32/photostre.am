2ef01ec0a4029a45644ca14f360a677ee3215614
Thomas R. Koll <tomk32@tomk32.de>
2009-09-03 00:43:50 +0200

    Using make_resourceful in websites controller

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,44 +1,17 @@
 class WebsitesController < ApplicationController
-  before_filter :set_website, :only => [:edit, :update]
+  make_resourceful do
+    actions :all
+    belongs_to :current_user
 
-  def index
-    @websites = current_user.websites
-  end
-
-  def new
-    @website = current_user.websites.new(params[:website])
-    @website.domain ||= [current_user.login.downcase, current_website.domain].join('.')
-  end
-
-  def edit
-  end
-
-  def create
-    @website = Website.new(params[:website])
-    if @website.save
-      @website.users << current_user
-      redirect_to :action => :index and return
-    else
-      flash[:error] = "Couldn't create website"
-      render :new and return
+    after :create do
+      current_object.users << current_user
     end
-  end
 
-  def update
-    @website.attributes = params[:website]
-    if @website.save
-      redirect_to :action => :index and return
-    else
-      flash[:error] = "Couldn't save website"
-      render :edit and return
-    end
-  end
-
-  def set_website
-    @website = Website.find(params[:id])
-    if ! @website.users.include?(current_user)
-      flash[:error] = "You are not allowed to edit this website"
-      redirect_to :action => :index and return
+    before :edit, :update, :show do
+      if ! @current_object.new_record? and ! @current_object.users.include?(current_user)
+        flash[:error] = "You are not allowed to edit this website"
+        redirect_to :action => :index
+      end
     end
   end
 end
