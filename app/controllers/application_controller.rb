e1ea50eb65dda6e4fdc89fc95cbf26dcd89c1b89
Thomas R. Koll <tomk32@tomk32.de>
2009-10-20 08:34:25 +0200

    Excluded setting of theme to call it as a before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include Authentication
-  
+  before_filter :set_theme
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '67063682bba404156fa25ca1e1f99060'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   filter_parameter_logging :password
 
   protected
@@ -24,16 +24,19 @@ class ApplicationController < ActionController::Base
     end
     @current_website ||= Website.active.first
     return false unless @current_website
-    # use the websites theme for views
-    self.prepend_view_path File.join(current_theme_path, 'views')
     return(@current_website)
   end
   helper_method :current_website
-  
+
+  def set_theme
+    # use the websites theme for views
+    self.prepend_view_path File.join(current_theme_path, 'views')
+  end
+
   def current_theme_path
     File.join(RAILS_ROOT, 'themes', 'websites', current_website.domain, current_website.theme)
   end
-  
+
   def current_album
     return if params[:album_id].nil?
     @current_album ||= current_website.albums.published.find_by_permalink!(params[:album_id])
