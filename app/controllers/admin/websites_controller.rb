39c60ccacdae390f500b5449977247c19991d010
Thomas R. Koll <tomk32@tomk32.de>
2009-11-03 08:40:05 +0100

    Implemented adding domains and subdomains

--- app/controllers/admin/websites_controller.rb
+++ app/controllers/admin/websites_controller.rb
@@ -13,8 +13,19 @@ class Admin::WebsitesController < Admin::ApplicationController
         redirect_to :action => :index
       end
     end
+
+    # combine params subdomain and domain to params[:website][:domain]
+    before :create do
+      unless params[:subdomain].blank? && params[:domain].blank?
+        if Website.system.find_by_domain(params[:domain])
+          current_object.domain = params[:subdomain] + '.' + params[:domain]
+        elsif current_user.websites.find_by_domain(params[:domain])
+          current_object.domain = params[:subdomain] + '.' + params[:domain]
+        end
+      end
+    end
   end
-    
+
   def current_objects
     @current_objects ||= current_user.websites.find(:all)
   end
--- spec/controllers/admin/website_controller_spec.rb
+++ spec/controllers/admin/website_controller_spec.rb
@@ -12,6 +12,12 @@ describe Admin::WebsitesController do
     get :index
     controller.should have_before_filter(:authenticated)
   end
+  describe ":index" do
+    it "should be success" do
+      get :index
+      response.should be_success
+    end
+  end
   describe ":new without sufficient subscription" do
     it "should redirect to subscriptions"
   end
@@ -21,7 +27,44 @@ describe Admin::WebsitesController do
     it "should not allow setting theme created by other users"
   end
   describe ":create with subscription" do
-    it "should create a new website"
+    it "should create a new website" do
+      post :create, {:website => {:site_title => 'Test', :domain => 'tomk32.de'}}
+      response.should be_redirect
+      assigns[:current_object].should be_valid
+      assigns[:current_object].domain.should ==('tomk32.de')
+      assert @user.websites.find_by_domain('tomk32.de')
+    end
+    it "should use create a new subdomain under system domain" do
+      Factory(:website, :domain => 'funny.com', :state => 'system')
+      assert_nil @user.websites.find_by_domain('test.funny.com')
+      post :create, {:subdomain => 'test', :domain => 'funny.com', :website => {:site_title => 'Test'}}
+      response.should be_redirect
+      assigns[:current_object].should be_valid
+      assigns[:current_object].domain.should ==('test.funny.com')
+      assert @user.websites.find_by_domain('test.funny.com')
+    end
+    it "should create a new subdomain under a user domain" do
+      @user.websites << Factory(:website, :domain => 'user.com')
+      assert_nil @user.websites.find_by_domain('test.user.com')
+      post :create, {:subdomain => 'test', :domain => 'user.com', :website => {:site_title => 'Test'}}
+      response.should be_redirect
+      assigns[:current_object].should be_valid
+      assigns[:current_object].domain.should ==('test.user.com')
+      assert @user.websites.find_by_domain('test.user.com')
+    end
+    it "should not create a new subdomain under a non-existing domain" do
+      assert_nil @user.websites.find_by_domain('test.bad.com')
+      post :create, {:subdomain => 'test', :domain => 'bad.com', :website => {:site_title => 'Test'}}
+      response.should_not be_success
+      assigns[:current_object].should_not be_valid
+    end
+    it "should not create a new subdomain under someone elses domain" do
+      other_website = Factory(:website, :domain => 'theirs.com', :users => [Factory(:user)])
+      assert_nil @user.websites.find_by_domain('theirs.com')
+      post :create, {:subdomain => 'test', :domain => 'theirs.com', :website => {:site_title => 'Test'}}
+      response.should_not be_success
+      assigns[:current_object].should_not be_valid
+    end
   end
   describe ":destroy" do
     it "should set state to deleted"
