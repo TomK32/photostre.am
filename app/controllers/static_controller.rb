d202f550d9ea45412cfc2811ddf3ac78c90713af
Thomas R. Koll <tomk32@tomk32.de>
2009-11-05 13:10:21 +0100

    Kicking out system websites from the "newest" on static/index

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def index
-    @websites = Website.active.latest.all(:limit => 5)
+    @websites = Website.latest.all(:limit => 5, :conditions => {:state => 'active'})
     @albums = Album.latest.published.all(:limit => 5)
   end
 end
