4686b36957c8a3781e57c3cf149fd5f8ef20dc21
Thomas R. Koll <tomk32@tomk32.de>
2009-11-12 09:02:48 +0100

    Working photos views specs

--- spec/views/photos_views_spec.rb
+++ spec/views/photos_views_spec.rb
@@ -10,10 +10,24 @@ describe "photos/index" do
   it "should display photos" do
     response.should be_success
     response.should have_tag('div#photos')
-    @website.photos.each do |photo|
-      response.should have_tag("div#photo_%s" % photo.id)
+    @photos.each do |photo|
+      response.should have_tag("div#photo_%s.photo" % photo.id)
     end
-    response.should have_tag('div.pagination')
+  end
+  it "should have pagination" do
+    response.should have_tag('div.pagination .current')
   end
 end
 
+describe "photos/show" do
+  before :each do
+    @website = Factory(:website_with_photos)
+    @photo = @website.photos[4]
+    template.stub(:current_object).and_return(@photo)
+    render
+  end
+  it "should display photo" do
+    response.should be_success
+    response.should have_tag('div#photo_%i.photo' % @photo.id)
+  end
+end
