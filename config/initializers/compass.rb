5fea966cfbf323a54a845afb4c045f31d7d8c06c
Thomas R. Koll <tomk32@tomk32.de>
2009-12-18 00:33:59 +0100

    * Fixed layout for admin * Tiny changes about CSS

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 require 'compass'
 # If you have any compass plugins, require them here.
 Compass.configuration.parse(File.join(Rails.root, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
+Compass.configuration.environment = :production 
 Compass.configuration.css_dir = File.join('themes', 'default', 'public', 'stylesheets')
 Compass.configure_sass_plugin!
