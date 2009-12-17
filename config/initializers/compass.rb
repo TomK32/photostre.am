548b771aadce8f75c8841275b1b886c81de7f439
Thomas R. Koll <tomk32@tomk32.de>
2009-12-18 00:12:04 +0100

    Basically all websites, including the system websites run on themes now. * SASS saves it's output in the default theme's path now * Updated application layout for default theme

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,6 @@
 require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+Compass.configuration.parse(File.join(Rails.root, "config", "compass.config"))
 Compass.configuration.environment = RAILS_ENV.to_sym
+Compass.configuration.css_dir = File.join('themes', 'default', 'public', 'stylesheets')
 Compass.configure_sass_plugin!
