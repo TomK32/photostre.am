4a6adc8a38cc961ff8127731025008226b3679a6
Thomas R. Koll <tomk32@tomk32.de>
2009-12-18 10:07:02 +0100

    To make things easier, the domain of websites can't be changed

--- spec/views/admin/websites_views_spec.rb
+++ spec/views/admin/websites_views_spec.rb
@@ -7,24 +7,32 @@ describe "admin/websites/new" do
     @website = Factory(:website, :domain => 'user.com')
     @website.users << @user
     @photos = @website.photos.paginate(:per_page => 5, :page => 1)
-    template.stub(:current_object).and_return(Website.new)
     template.stub(:objects_path).and_return('/admin/websites')
-    render
+    template.stub(:current_object).and_return(Website.new)
   end
-  it "should have fields" do
+  it "should have fields for existing website" do
+    template.stub(:current_object).and_return(Factory(:website))
+    render
     response.should be_success
     response.should have_form_posting_to('/admin/websites')
     response.should have_text_field_for :website, :site_title
     response.should have_text_field_for :website, :meta_keywords
     response.should have_text_field_for :website, :description
-    response.should have_text_field_for :website, :domain
+    response.should_not have_text_field_for :website, :domain
     response.should have_text_area_for :website, :tracking_code
     # TODO missing or maybe buggy matcher
     #response.should have_select_for :domain
+    response.should_not have_tag('input#subdomain')
+    response.should_not have_tag('select#domain')
+  end
+  it "should have fields for new website" do
+    template.stub(:current_object).and_return(Website.new)
+    render
     response.should have_tag('input#subdomain')
     response.should have_tag('select#domain')
   end
   it "should display system domains in drop down" do
+    render
     response.should have_tag('select#domain') do |tag|
       # FIXME
       tag.should have_tag('option')
