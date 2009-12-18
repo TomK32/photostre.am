69eec02a2170fe051a05e61cea20e39968f31d43
Thomas R. Koll <tomk32@tomk32.de>
2009-12-18 16:12:08 +0100

    Added raphael and some graph on the statistics

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,7 @@ class StaticController < ApplicationController
     @websites = Website.latest.all(:limit => 5, :conditions => {:state => 'active'})
     @albums = Album.latest.published.all(:limit => 5)
   end
+  def statistics
+    redirect_to root_path unless logged_in? and current_user.login == 'TomK32'
+  end
 end
