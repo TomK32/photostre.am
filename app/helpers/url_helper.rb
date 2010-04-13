409b6d53e8c09600f93b61ec1064b808ff5e2973
root <root@bananenpink.(none)>
2010-04-13 17:40:13 +0000

    Fixed url_helper

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,14 +1,14 @@
 module UrlHelper
-  def album_url_with_host(album, *options)
-    album_url(album.permalink, :host => album.website.domain, *options)
+  def album_url_with_host(album, options = {})
+    album_url(album.permalink, options.merge(:host => album.website.domain))
   end
-  def photo_url_with_host(photo, *options)
+  def photo_url_with_host(photo, options = {})
     if options.first.is_a?(Hash) and options.first[:album]
       album = options.first.delete(:album)
       options << {:host => album.website.domain}
-      album_photo_url(album.permalink, photo.permalink, *options)
+      album_photo_url(album.permalink, photo.permalink, options)
     else
-      photo_url(photo.permalink, *options)
+      photo_url(photo.permalink, options)
     end
   end
 end
