ba025ed0dd2f3fdb6353554aa121905df91b23a0
Thomas R. Koll <tomk32@tomk32.de>
2010-04-13 19:53:45 +0200

    Updated admin/theme_controller for inherit_resources

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -1,12 +1,5 @@
 class Admin::ThemesController < Admin::ApplicationController
-  make_resourceful do
-    actions :all
-    before :create do
-      current_object.author ||= current_user
-    end
-  end
+  inherit_resources
+  actions :all
   
-  def current_objects
-    scope = Theme.paginate(pagination_defaults)
-  end
 end
