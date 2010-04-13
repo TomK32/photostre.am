a70fed73670c61706dd32ef4b2eacb05ce725de1
Thomas R. Koll <tomk32@tomk32.de>
2010-04-13 19:56:43 +0200

    Updated albums_controller to use inherit_resources

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,21 +1,25 @@
 class AlbumsController < ApplicationController
-  make_resourceful do
-    actions :all
-    before :show do
-      return if current_object.nil?
-      @photos = current_object.photos.published.paginate(pagination_defaults)
-    end
-  end
-  def current_objects(per_page = 5)
-    scope = current_website.albums.published
-    @current_objects ||= scope.paginate(pagination_defaults)
+  inherit_resources
+  actions :all
+  def show
+    render '404' if current_object.nil?
+    @photos = current_object.photos.published.paginate(pagination_defaults)
+    show!
   end
 
-  def current_object
+  private
+  def collection
+    return @collection if @collection
+    scope = current_website.albums.published
+    @collection ||= scope.paginate(pagination_defaults)
+  end
+
+  def resource
+    return @resource if @resource
     return false if params[:id].blank?
     scope = current_website.albums.published
-    @current_object ||= scope.find_by_permalink!(params[:id])
-    @current_object ||= scope.find(params[:id])
-    @current_object
+    @resource ||= scope.find_by_permalink!(params[:id])
+    @resource ||= scope.find(params[:id])
+    @resource
   end
 end
