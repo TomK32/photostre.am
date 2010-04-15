829f3a180e68eef56f305fbd82b4add5300d8d43
Thomas R. Koll <tomk32@tomk32.de>
2010-04-15 23:00:45 +0200

    Refactored the whole page_title thing, a lot less code now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   attr_accessor :title, :title_in_content
-
   def title(text, in_content = true)
     @title = text
     @title_in_content = in_content
   end
 
-  def page_title
+  def page_title(string = nil)
+    string ||= :"#{controller_name}.#{action_name}.page_title"
+    if @title.blank? and defined?(resource) and resource.respond_to?(:title)
+      @title = t(string, :title => resource.title, :default => resource.title)
+    else
+      @title = t(string, :default => @title)
+    end
     [@title, (current_website.title || t(:'app.title'))].reject{|a|a.blank?}.compact.join(' // ')
   end
 
