0767c028d3e6b579dbcb1a9d7d5210b9518836a3
Thomas R. Koll <tomk32@tomk32.de>
2010-04-20 11:20:57 +0200

    Fix: readded check for params[:id] to meta_tags helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def meta_tags(object = nil)
-    object ||= resource if defined?(resource) and !resource.nil?
+    object ||= resource if defined?(resource) and params[:id] and !resource.nil?
     return if object.nil?
     [:meta_geourl, :meta_keywords, :meta_description].each do |method|
       if object.respond_to?(method) and ! object.send(method).blank?
