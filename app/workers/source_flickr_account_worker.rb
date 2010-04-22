424b4ef0bbf6d6e4b849f185d028886d4e249083
Thomas R. Koll <tomk32@tomk32.de>
2010-04-22 16:39:34 +0200

    Stripped down SourceFlickrAccountWorker to its basics

--- app/workers/source_flickr_account_worker.rb
+++ app/workers/source_flickr_account_worker.rb
@@ -1,14 +1,6 @@
 class SourceFlickrAccountWorker
   def self.update_data(options)
     source = User.where({'sources._id' => options[:id]}).first.sources.find(options[:id])
-    return if source.updating?
-    begin
-      source.update_data
-    rescue RuntimeError => ex
-      # possibly run out of memory, root knows
-      # but just let's kick another workling
-      source.update_attributes(:status => 'active')
-      source.call_worker
-    end
+    source.update_data
   end
 end
