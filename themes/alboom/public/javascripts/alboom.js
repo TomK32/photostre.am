3e90a370d0a6ea60c2e88e960eb9467cf7051184
Thomas R. Koll <tomk32@tomk32.de>
2010-04-27 11:46:43 +0200

    Started theme Alboom

---
+++ themes/alboom/public/javascripts/alboom.js
@@ -0,0 +1,13 @@
+// we kinda assume that the target has a sibling after it to get a 
+function fitContentSize(target) {
+  // 100 for the photos in the footer
+  max_image_height = $(window).height() - $(target).offset().top  - ($('body').height() - $(target).next().offset().top );
+  $('img', target).height(Math.min($('img', target).attr('naturalHeight'), max_image_height - 80));
+  $(target).height(max_image_height);
+  $(target).width(Math.max(300, $('img', target).width()));
+}
+
+$(document).ready(function(){
+  $(window).bind('resize', function(event){ fitContentSize('.large'); });
+  $(window).bind('load', function(event){ fitContentSize('.large'); });
+});
