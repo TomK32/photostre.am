c3d6b493fb7b50b5e2f18ce7bd8cb6e4fe561bf7
Thomas R. Koll <tomk32@tomk32.de>
2010-04-27 21:05:51 +0200

    Working on alboom theme

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,7 +1,7 @@
 module PhotosHelper
   def source_link(photo)
     if photo.source
-      raw link_to(@photo.source.source_title, @photo.web_url)
+      raw link_to(photo.source.source_title, photo.web_url)
     end
   end
 end
