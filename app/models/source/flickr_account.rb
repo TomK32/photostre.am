5e2db74dd388058549cb701c256cc96228b72b73
Thomas R. Koll <tomk32@tomk32.de>
2010-05-04 10:49:05 +0200

    Changed way source status is set so we don't have 100s of jobs if the worker is somewhy down

--- app/models/source/flickr_account.rb
+++ app/models/source/flickr_account.rb
@@ -86,14 +86,14 @@ class Source::FlickrAccount < Source
   def call_worker
 #    return if self.deleted?
     return unless self.authenticated?
+    return unless active?
+    self.update_attributes(:status => 'updating')
     Navvy::Job.enqueue(SourceFlickrAccountWorker, :update_data, {:id => self.id})
   end
 
   # TODO import and create albums
   def update_data
     return unless authenticated?
-    return unless active?
-    self.update_attributes(:status => 'updating')
     begin
       page = 0
       per_page = 200
