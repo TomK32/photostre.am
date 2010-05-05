27403671058fdcbe2f2336a5e01328b5611459a2
Thomas R. Koll <tomk32@tomk32.de>
2010-05-05 16:16:53 +0200

    Different arrows for next/previous links in photomanager

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,10 +37,10 @@ module ApplicationHelper
   def next_previous_links(collection, separator = " ")
     result = []
     if collection.previous_page
-      result << link_to('«', {:page => collection.previous_page}, :class => 'previous')
+      result << raw(link_to('<<', {:page => collection.previous_page}, :class => 'previous'))
     end
     if collection.next_page
-      result << link_to('»', {:page => collection.next_page}, :class => 'next')
+      result << raw(link_to('>>', {:page => collection.next_page}, :class => 'next'))
     end
     return result.join(separator)
   end
