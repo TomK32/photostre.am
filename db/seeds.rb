dbedf1575ac1abb1b89675de11defd9927878747
Thomas R. Koll <tomk32@tomk32.de>
2010-05-05 21:59:59 +0200

    * Added Grash theme * Added Quicksand font

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-
+Website.create!(:site_title => 'photostre.am', :domain => 'photostre.am', :state => 'system')
 admin = User.first || User.create!(:login => 'admin')
 Theme.create!(:name => 'Alboom', :directory => 'alboom', :author => admin, :status => 'public',
   :description => 'Minimalistic white theme.')
 Theme.create!(:name => 'Blick', :directory => 'blick', :author => admin, :status => 'public',
   :description => 'Vintage green with dark blue headers and links in a minimalistic design.')
+Theme.create!(:name => 'Grash', :directory => 'grash', :author => admin, :status => 'public',
+  :description => 'Futuristic grey and blue nightmare.')
