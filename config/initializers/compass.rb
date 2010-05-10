a2ef89894e9d53f269ab05b2a0b6430bbf37a8c8
Thomas R. Koll <tomk32@tomk32.de>
2010-05-10 14:14:55 +0200

    Fixed up config for Compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(Rails.root, "config", "compass.config"))
+Compass.add_project_configuration(Rails.root.join("config", "compass.config"))
 Compass.configuration.environment = :production
 Compass.configuration.css_dir = File.join('themes', 'system', 'public', 'stylesheets')
 Compass.configure_sass_plugin!
