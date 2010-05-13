0adae23a28cced4c3b2229c8b70ad5b1b36d60b6
Thomas R. Koll <tomk32@tomk32.de>
2010-05-13 13:31:14 +0200

    Show a public list of themes

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -1,11 +1,21 @@
 class Admin::ThemesController < Admin::ApplicationController
   inherit_resources
   actions :all
+  skip_before_filter :authenticated, :only => :index
 
-  
+  def index
+    index! {
+      render :layout => 'application' and return
+    }
+  end
   def create
     resource = build_resource
     resource.author ||= current_user
     create!
+  end
+
+  private
+  def collection
+    @themes ||= Theme.public.paginate(pagination_defaults)
   end
 end
