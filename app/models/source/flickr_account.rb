557bf5fd9e74e27ac4335d3fcbf368e4081be63e
Thomas R. Koll <tomk32@tomk32.de>
2010-05-16 13:43:20 +0200

    Rescue flickr errors in import_albums too

--- app/models/source/flickr_account.rb
+++ app/models/source/flickr_account.rb
@@ -8,7 +8,7 @@ class Source::FlickrAccount < Source
   validates_presence_of :flickr_nsid
   after_create :call_worker
   before_validate :set_title
-  
+
   def validate_on_create
     errors.add 'username', 'not a flickr account' unless set_nsid
   end
@@ -46,7 +46,7 @@ class Source::FlickrAccount < Source
     options.merge!({:token => Flickr::Auth::Token.new(token)}) unless token.blank?
     @flickr = Flickr.new(options)
   end
-  
+
   def photostream_url
     "http://www.flickr.com/photos/%s" % flickr_nsid
   end
@@ -62,15 +62,15 @@ class Source::FlickrAccount < Source
   def before_validation
     self.title = self.username if self.title.blank?
   end
-  
+
   def authentication_url
     flickr.auth.url(:read)
   end
-  
+
   def authenticated?
-    ! (token.blank? or authenticated_at.blank?) 
+    ! (token.blank? or authenticated_at.blank?)
   end
-  
+
   def authenticate(frob)
     flickr.auth.frob = frob
     unless flickr.auth.token.nil? or flickr.auth.token.token.blank?
@@ -82,7 +82,7 @@ class Source::FlickrAccount < Source
     end
     return false
   end
-  
+
   def call_worker
 #    return if self.deleted?
     return unless self.authenticated?
@@ -94,59 +94,48 @@ class Source::FlickrAccount < Source
   # TODO import and create albums
   def update_data
     return unless authenticated?
-    begin
+    execute_with_rescue do
       page = 0
       per_page = 200
+      flickr.photos.extras.merge!({:url_o => :original_url, :description => :description,
+          :original_secret => :original_secret})
       begin
-        flickr.photos.extras.merge!({:url_o => :original_url, :description => :description,
-            :original_secret => :original_secret})
-        begin
-          page += 1
-          flickr_photos = flickr.photos.search(:per_page => per_page, :page => page,
-              :user_id => 'me', :auth_token => self.token)
-          flickr_photos_count = flickr_photos.size
-          existing_photos = Photo.where(:source_id => self.id,
-              :remote_id.in => flickr_photos.collect{|p| p.id }
-            ).only(:remote_id).collect{|p| p.remote_id.to_s }
-          flickr_photos.reject!{|p| existing_photos.include?(p.id.to_s) }
+        page += 1
+        flickr_photos = flickr.photos.search(:per_page => per_page, :page => page,
+            :user_id => 'me', :auth_token => self.token)
+        flickr_photos_count = flickr_photos.size
+        existing_photos = Photo.where(:source_id => self.id,
+            :remote_id.in => flickr_photos.collect{|p| p.id }
+          ).only(:remote_id).collect{|p| p.remote_id.to_s }
+        flickr_photos.reject!{|p| existing_photos.include?(p.id.to_s) }
 
-          flickr_photos.each do |photo|
+        flickr_photos.each do |photo|
 
-            next if photo.media != 'photo'
-            photo_attr = {
-              :title => photo.title,
-              :remote_id => photo.id,
-              :taken_at => photo.taken_at,
-              :created_at => photo.uploaded_at,
-              :updated_at => photo.updated_at,
-              :tags => photo.tags.split(' '),
-              :web_url => 'http://www.flickr.com/photos/%s/%s' % [self.flickr_nsid, photo.id],
-              :machine_tags => photo.machine_tags.split(' '),
-              :description => photo.description,
-              :photo_urls => {
-                  :o => photo.original_url,
-                  :m => photo.url(:medium),
-                },
-              :public => photo.public?,
-              :friend => photo.friend?,
-              :family => photo.family?,
-              :original_secret => photo.original_secret,
-              :user_id => self.user.id,
-              :source_id => self.id
-            }
-            Photo.create(photo_attr)
-          end
-        return
-        end while page < flickr_photos.page
-      end
-    rescue Flickr::Error => ex
-      self.error_messages ||= []
-      self.error_messages << {:date => Time.now, :msg => ex.message}
-      if ex.message.match(/(User not found|Invalid auth token)/)
-        self.status = 'inactive'
-      end
-      self.save!
-      return
+          next if photo.media != 'photo'
+          photo_attr = {
+            :title => photo.title,
+            :remote_id => photo.id,
+            :taken_at => photo.taken_at,
+            :created_at => photo.uploaded_at,
+            :updated_at => photo.updated_at,
+            :tags => photo.tags.split(' '),
+            :web_url => 'http://www.flickr.com/photos/%s/%s' % [self.flickr_nsid, photo.id],
+            :machine_tags => photo.machine_tags.split(' '),
+            :description => photo.description,
+            :photo_urls => {
+                :o => photo.original_url,
+                :m => photo.url(:medium),
+              },
+            :public => photo.public?,
+            :friend => photo.friend?,
+            :family => photo.family?,
+            :original_secret => photo.original_secret,
+            :user_id => self.user.id,
+            :source_id => self.id
+          }
+          Photo.create(photo_attr)
+        end
+      end while page < flickr_photos.pages
     end
     self.update_attributes(:status => 'active')
   end
@@ -154,15 +143,33 @@ class Source::FlickrAccount < Source
   def import_albums
     # TODO this is quite expensive as I think it writes all alubms again on reimport
     # but otoh we don't do it that often.
-    photosets = flickr.photosets.get_list(:user_id => self.flickr_nsid, :auth_token => self.token)
-    photosets.each do |photoset|
-      album = self.albums.where(:remote_id => photoset.id).first ||
-        Source::FlickrAccount::Album.new({:title => photoset.title, :remote_id => photoset.id.to_i,
-          :description => photoset.description, :remote_photo_ids => []})
-      album.remote_photo_ids = photoset.get_photos(:user_id => self.flickr_nsid, :auth_token => self.token).collect{|p|p.id.to_i}
-      self.albums << album
+    execute_with_rescue do
+      photosets = flickr.photosets.get_list(:user_id => self.flickr_nsid, :auth_token => self.token)
+      photosets.each do |photoset|
+        album = self.albums.where(:remote_id => photoset.id).first ||
+          Source::FlickrAccount::Album.new({:title => photoset.title, :remote_id => photoset.id.to_i,
+            :description => photoset.description, :remote_photo_ids => []})
+        album.remote_photo_ids = photoset.get_photos(:user_id => self.flickr_nsid, :auth_token => self.token).collect{|p|p.id.to_i}
+        self.albums << album
+      end
+      self.save!
     end
-    self.save!
+  end
+
+  # Rescues any Flickr errors that might come up in the block
+  # and save them in the account's error_messages
+  def execute_with_rescue(&block)
+    begin
+      yield block
+    rescue Flickr::Error => ex
+      self.error_messages ||= []
+      self.error_messages << {:date => Time.now, :msg => ex.message}
+      if ex.message.match(/(User not found|Invalid auth token)/)
+        self.status = 'inactive'
+      end
+      self.save
+      return
+    end
   end
 end
 
