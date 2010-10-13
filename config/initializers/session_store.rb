4bc5288001a7144e0231dd200ed147450dbb22c4
Thomas R. Koll <tomk32@tomk32.de>
2010-10-13 20:52:33 +0200

    Removing i18n initalizer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, :key => '_das-photowall_session', :expire_after => 1.month
+DasPhotowall::Application.config.secret_token = '84f3c2d01ef22e39fadb7a93fab0f36e4c5d1068850e2ef194135416ad105e823101d8a512fb4041be021367ff28fd76a051cb0c532d0b8d326de9cc9ce08df5'
+
+DasPhotowall::Application.config.session_store :cookie_store, :key => '_das_photowall_session', :expire_after => 1.month
+
